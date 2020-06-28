127801321a33824da0967c3b97aec439b1ac4fe7
Dmitry Trofimov <tdmitriiv@gmail.com>
2020-06-28 14:26:54 +0400

    improvement : homepage setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
-  <div></div>
+  <div>Homepage</div>
 </template>
 
-<script>
-export default {
-  name: 'Home',
-};
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Page } from '@/types/entities';
+
+@Component
+export default class Home extends Vue {
+  pages: Array<Page> = [];
+}
 </script>
